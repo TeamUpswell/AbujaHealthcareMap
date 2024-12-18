336c619971463414b72333bfa5e7fcf97075468b
Drew Bernard <82413471+TeamUpswell@users.noreply.github.com>
2024-12-18 11:27:31 -0800

    First push

--- data/pharmacies.js
+++ data/pharmacies.js
@@ -1,50 +1,134 @@
 pharmacies = [
     {
-        "name": "Hajjo Yawo Pharmacy",
-        "address": "Jimeta, Yola, Adamawa",
-        "latitude": 9.261733993,
-        "longitude": 12.45380743
+        "name": "Em-Zik pharmacy & Supermarket",
+        "address": "Lugbe Zone 5, Along Airport Road, Lugbe, Lugbe 900107, Federal Capital Territory, Nigeria",
+        "latitude": 8.9753469,
+        "longitude": 7.3756843
     },
     {
-        "name": "Hakuri Pharmacy Bekaji",
-        "address": "Bekaji Corner, Adjacent Bekaji Primary School",
-        "latitude": 9.2618049,
-        "longitude": 12.4443273
+        "name": "SONTALLY PHARMACY",
+        "address": "315 PHASE 2 ACO/AMAC HOUSING ESTATE, AIRPORT ROAD, ABUJA",
+        "latitude": 8.9450526,
+        "longitude": 7.2872428
     },
     {
-        "name": "Hakuri Pharmacy Jimeta",
-        "address": "80 Mohammed Mustafa Road, Jimeta, Adamawa",
-        "latitude": 9.2797299,
-        "longitude": 12.4584676
+        "name": "Befit Pharmacy",
+        "address": "Chika entrance, Airport Road, Abuja.",
+        "latitude": 9.0015813,
+        "longitude": 7.4108297
     },
     {
-        "name": "Shekinah Pharmacy",
-        "address": "Adjacent Central Bank of Nigeria, Opposite First Bank, Bank Road, Jimeta",
-        "latitude": 9.2686101,
-        "longitude": 12.4439691
+        "name": "Sunamot Pharmacy",
+        "address": "ACO Estate, Airport Road, Abuja",
+        "latitude": 8.9428625,
+        "longitude": 7.324413
     },
     {
-        "name": "Balavin Pharmacy",
-        "address": "No 2/3 Army Barracks Road, Jimeta",
-        "latitude": 9.2716518,
-        "longitude": 12.4528907
+        "name": "Zinni Pharmacy Ltd.",
+        "address": "Lugbe Plaza, Lugbe 900107, Federal Capital Territory, Nigeria",
+        "latitude": 8.997675,
+        "longitude": 7.3496131
     },
     {
-        "name": "Jimeta Central Pharmacy",
-        "address": "148 Mohammed Mustapha Way, Yola",
-        "latitude": 9.2034963,
-        "longitude": 12.4953899
+        "name": "Imppro Pharmaceuticals LTD (Medicare Marche)",
+        "address": "95, Trademore Estate, Lugbe, Abuja",
+        "latitude": 8.9571242,
+        "longitude": 7.367954
     },
     {
-        "name": "Panyi Pharmacy Ltd",
-        "address": "Bajabure Federal Housing Estate, Girei",
-        "latitude": 9.3222588,
-        "longitude": 12.4774567
+        "name": "Morden Times Pharmacy",
+        "address": "Penthouse Estate, Lugbe, Abuja",
+        "latitude": 8.9650653,
+        "longitude": 7.3833023
     },
     {
-        "name": "L&L Pharmacy",
-        "address": "KR Paktajoma Plaza, Bachure Road, Jimeta-Yola South, Behind Jezco Oil",
-        "latitude": 9.2425304,
-        "longitude": 12.4196523
+        "name": "Datura Pharmacy Abuja",
+        "address": "Beside Customary Court, Galadimawa village",
+        "latitude": 8.9927126,
+        "longitude": 7.4239399
+    },
+    {
+        "name": "LILIFIED PHARMACY",
+        "address": "SUNNY VALE ESTATE LOKOGOMA",
+        "latitude": 8.9817433,
+        "longitude": 7.4376037
+    },
+    {
+        "name": "Sante Bene Rx",
+        "address": "Gudu Junction Behind Nnpc Apo, Abuja, FCT",
+        "latitude": 9.0008359,
+        "longitude": 7.4720833
+    },
+    {
+        "name": "Jomed Pharmacy",
+        "address": "Apo Sunrise Plaza, Apo Resettlement Abuja",
+        "latitude": 8.9870662,
+        "longitude": 7.4983672
+    },
+    {
+        "name": "Emmitex Medical Store",
+        "address": "Wumba, behind His Grace pavilion estate, Apo, Abuja",
+        "latitude": 8.9668559,
+        "longitude": 7.4863656
+    },
+    {
+        "name": "ShowCare Pharmacy",
+        "address": "Loko 10 stores, Lokogoma",
+        "latitude": 8.969236,
+        "longitude": 7.4760925
+    },
+    {
+        "name": "Cmargs Pharmacy",
+        "address": "First avenue, Efab Estate shop 1, Lokogoma, Abuja",
+        "latitude": 9.0545309,
+        "longitude": 7.4013162
+    },
+    {
+        "name": "MAC-CHIZ PHARMACY AND STORES LTD KUJE",
+        "address": "Uwaa Plaza, Beside First Bank, Kuje, Abuja",
+        "latitude": 8.8821438,
+        "longitude": 7.2275336
+    },
+    {
+        "name": "Mac-Chiz Pharmacy Pegi",
+        "address": "JOHNSLOT PLAZA, PEGI ESTATE, AFTER 1000 UNIT GATE KUJE ABUJA",
+        "latitude": 8.8291304,
+        "longitude": 7.2683492
+    },
+    {
+        "name": "Nesterglaz Pharmacy",
+        "address": "JIKwoyi ROAD",
+        "latitude": 8.9842515,
+        "longitude": 7.5638474
+    },
+    {
+        "name": "Elot Pharmacy",
+        "address": "XGP2+MCV, Apo, Abuja 900110, Federal Capital Territory, Nigeria",
+        "latitude": 8.9867375,
+        "longitude": 7.5010469
+    },
+    {
+        "name": "Bamak Pharmacy",
+        "address": "House 8, Zone D. Apo Resettlement, Abuja",
+        "latitude": 8.9694692,
+        "longitude": 7.5045683
+    },
+    {
+        "name": "Keecee Pharmacy",
+        "address": "Opposite Catholic church, Kabusa Village Abuja",
+        "latitude": 9.0562646,
+        "longitude": 7.4985259
+    },
+    {
+        "name": "Beta Pharmacy",
+        "address": "Shop 2 Area 2 Shopping Complex Garki",
+        "latitude": 9.0356843,
+        "longitude": 7.4778649
+    },
+    {
+        "name": "Pyramid Pharmacy",
+        "address": "73 Samuel Ladoke Akintola Boulevard, Garki",
+        "latitude": 9.020915,
+        "longitude": 7.4855361
     }
 ];
--- data/phcs.js
+++ data/phcs.js
@@ -304,5 +304,529 @@ const healthcareCenters = [
         "hours_of_work": "",
         "latitude": 9.1957937,
         "longitude": 12.4819925
+    },
+
+    // New Bwari, Kuje, and AMAC Primary Health Centers
+    {
+        "name": "Kogo Phc",
+        "lga": "Bwari",
+        "address": "794q+C7x, Bwari 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2561125,
+        "longitude": 7.3882344
+    },
+    {
+        "name": "Byazhin Phc",
+        "lga": "Bwari",
+        "address": "58cg+Fw7, Abuja 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1711625,
+        "longitude": 7.3272969
+    },
+    {
+        "name": "Chikachore Phc",
+        "lga": "Bwari",
+        "address": "58GV+F52, Kubwa, 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1761375,
+        "longitude": 7.3428906
+    },
+    {
+        "name": "Dutse Alhaji Phc",
+        "lga": "Bwari",
+        "address": "Zone 5 Bwari Express Road, Abuja 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1417045,
+        "longitude": 7.3726764
+    },
+    {
+        "name": "Dutse Makaranta Phc",
+        "lga": "Bwari",
+        "address": "Dutse Alhaji, 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1448362,
+        "longitude": 7.5355555
+    },
+    {
+        "name": "Mpape Phc",
+        "lga": "Bwari",
+        "address": "4fqv+Rcr, Mpape 901101 Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1396125,
+        "longitude": 7.4935156
+    },
+    {
+        "name": "Dawaki Phc",
+        "lga": "Bwari",
+        "address": "Opp Primary School, Dawaki 901101 Abuja.",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.132986,
+        "longitude": 7.3880964
+    },
+    {
+        "name": "Phc Deidei",
+        "lga": "Bwari",
+        "address": "Opp DeiDei Building Materials Market, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0993429,
+        "longitude": 7.2789214
+    },
+    {
+        "name": "Old Deidei",
+        "lga": "Bwari",
+        "address": "4775+Wq9, Deidei-Madalla Road, Opp Deidei Junction, Deidei, Madalla",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1147875,
+        "longitude": 7.2594844
+    },
+    {
+        "name": "B/Town Clinic",
+        "lga": "Bwari",
+        "address": "79w5+H76, Bwari 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2964125,
+        "longitude": 7.3581406
+    },
+    {
+        "name": "Kaduru Phc",
+        "lga": "Bwari",
+        "address": "79w5+H76, Bwari 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2964125,
+        "longitude": 7.3581406
+    },
+    {
+        "name": "S/Gari Phc",
+        "lga": "Bwari",
+        "address": "7957+Gc5, Bwari 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2587625,
+        "longitude": 7.3636094
+    },
+    {
+        "name": "Ushafa Phc",
+        "lga": "Bwari",
+        "address": "6c86+Vwg, Ushapa 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2171875,
+        "longitude": 7.4123281
+    },
+    {
+        "name": "Jigo Phc",
+        "lga": "Bwari",
+        "address": "69HQ+3PF, Jigo Ushafa, Bwari Area Council",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2855902,
+        "longitude": 7.3786689
+    },
+    {
+        "name": "N/Lay Out Phc",
+        "lga": "Bwari",
+        "address": "6c86+Vwg, Ushapa 901101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.2171875,
+        "longitude": 7.4123281
+    },
+    {
+        "name": "Chibiri Phc",
+        "lga": "Kuje",
+        "address": "Kuje 900105, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.8821438,
+        "longitude": 7.2275336
+    },
+    {
+        "name": "Lanto Phc",
+        "lga": "Kuje",
+        "address": "V692+X29, Shadadi 900105, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.8699125,
+        "longitude": 7.2001094
+    },
+    {
+        "name": "Gaube Phc",
+        "lga": "Kuje",
+        "address": "R7CJ+9MR, Pegi Estate, Pegi Village, Gaubi",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.8209875,
+        "longitude": 7.2816406
+    },
+    {
+        "name": "Gidan Bawa Phc",
+        "lga": "Kuje",
+        "address": "Holy Family Primary School Street, Kuje 900105, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.8821438,
+        "longitude": 7.2275336
+    },
+    {
+        "name": "Kusaki Phc",
+        "lga": "Kuje",
+        "address": "QCVM+C4M, Kusaki 900105, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.7935875,
+        "longitude": 7.4327969
+    },
+    {
+        "name": "Gudun Kaya Phc",
+        "lga": "Kuje",
+        "address": "F8WV+6C7, Buga 903102, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.4955375,
+        "longitude": 7.3435469
+    },
+    {
+        "name": "Zagabutu Phc",
+        "lga": "Kuje",
+        "address": "G6CJ+66X, Zagabuta 903102, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.5206125,
+        "longitude": 7.2306094
+    },
+    {
+        "name": "Kabikasa Phc",
+        "lga": "Kuje",
+        "address": "M8J7+4GF, Kabi 903102, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.6803125,
+        "longitude": 7.3137969
+    },
+    {
+        "name": "Kujekwa Phc",
+        "lga": "Kuje",
+        "address": "H9HH+2CR, Bambo 903102, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.5776125,
+        "longitude": 7.3785156
+    },
+    {
+        "name": "Kwaku Phc",
+        "lga": "Kuje",
+        "address": "M5M4+F2Q, Kvaku 904106, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.6837125,
+        "longitude": 7.1551094
+    },
+    {
+        "name": "Sabo Phc",
+        "lga": "Kuje",
+        "address": "Q5RX+C6R, Sabo 904106, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.7911125,
+        "longitude": 7.1980156
+    },
+    {
+        "name": "Kuchigoro Phc",
+        "lga": "AMAC",
+        "address": "LEA Primary School, Sabo Kukwaba 900108, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0161311,
+        "longitude": 7.4225245
+    },
+    {
+        "name": "Mabushi Phc",
+        "lga": "AMAC",
+        "address": "Community Market, Mabushi, Abuja 900108, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0818943,
+        "longitude": 7.4482243
+    },
+    {
+        "name": "Jahi Phc",
+        "lga": "AMAC",
+        "address": "3cwr+6f5, Kado, Ndawuse 900108, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0955125,
+        "longitude": 7.4412344
+    },
+    {
+        "name": "Jiwa Phc",
+        "lga": "AMAC",
+        "address": "38q4+936, Inner Northern Rd, Abuja 900106, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0906502,
+        "longitude": 7.3076626
+    },
+    {
+        "name": "Takushara Phc",
+        "lga": "AMAC",
+        "address": "VFJ5+HH, Takushara 900107, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.8814375,
+        "longitude": 7.4589375
+    },
+    {
+        "name": "Gidanmangoro Cphc",
+        "lga": "AMAC",
+        "address": "W987+P99, Lugbe 900107, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9167875,
+        "longitude": 7.3634844
+    },
+    {
+        "name": "Karu Phc",
+        "lga": "AMAC",
+        "address": "Chief Palace, karu village, by old, road",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0105329,
+        "longitude": 7.5727484
+    },
+    {
+        "name": "Lugbe Phc",
+        "lga": "AMAC",
+        "address": "Ummaru Musa Yar’adua Road, Lugbe 900107, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9450579,
+        "longitude": 7.2846625
+    },
+    {
+        "name": "Kabusa Phc",
+        "lga": "AMAC",
+        "address": "Lokogoma Rd, area, Abuja 900107, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.978043,
+        "longitude": 7.4629817
+    },
+    {
+        "name": "Gosa Phc",
+        "lga": "AMAC",
+        "address": "W7VX+3Q9, Airport Rd, Gosa 900102, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9426015,
+        "longitude": 7.2940009
+    },
+    {
+        "name": "Jikwoyi Phc",
+        "lga": "AMAC",
+        "address": "XHM7+PR2, Nyanya-Karshi Road, Phase 1, Jikwoyi 900101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9888466,
+        "longitude": 7.5668118
+    },
+    {
+        "name": "Pyakasa Phc",
+        "lga": "AMAC",
+        "address": "Xc82+4r8,Lugbe 900107, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9652875,
+        "longitude": 7.4020781
+    },
+    {
+        "name": "Waru Phc",
+        "lga": "AMAC",
+        "address": "Wfhp+7rf, 36 Ring Road 3, Waru 900109, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9281875,
+        "longitude": 7.4870469
+    },
+    {
+        "name": "Iddo Pada",
+        "lga": "AMAC",
+        "address": "X6p6+226, Idon Kasa 900102, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9850375,
+        "longitude": 7.2100156
+    },
+    {
+        "name": "Gishiri Phc",
+        "lga": "AMAC",
+        "address": "4f27+2wr, Kado, Abuja 900108, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1001125,
+        "longitude": 7.4647656
+    },
+    {
+        "name": "Aleyita Phc",
+        "lga": "AMAC",
+        "address": "X9qx+C8j, Aleyita Along, Airport Rd, Abuja",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9897816,
+        "longitude": 7.3975367
+    },
+    {
+        "name": "Garki Phc",
+        "lga": "AMAC",
+        "address": "2f9q+Pcw, Garki, Abuja 900110, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0193625,
+        "longitude": 7.4885781
+    },
+    {
+        "name": "Kagini Phc",
+        "lga": "AMAC",
+        "address": "47jx+3p2, Kagini Main Road, Abuja",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1301375,
+        "longitude": 7.2992656
+    },
+    {
+        "name": "Orozo Phc",
+        "lga": "AMAC",
+        "address": "VHWF+5XR, Orozo 900109, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.8954875,
+        "longitude": 7.5748906
+    },
+    {
+        "name": "Gbagalape Phc",
+        "lga": "AMAC",
+        "address": "3H3J+M49 Auditorium Gbagalape, Main St, AMAC 900101, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0541625,
+        "longitude": 7.5803594
+    },
+    {
+        "name": "Kpeyegyi Phc",
+        "lga": "AMAC",
+        "address": "Nyanya-Karshi Road, Kurudu 900109, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9423599,
+        "longitude": 7.5492012
+    },
+    {
+        "name": "Karshi Phc",
+        "lga": "AMAC",
+        "address": "Vmw4+72p, Boundaryline 961104, Nasarawa",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.8957125,
+        "longitude": 7.6550781
+    },
+    {
+        "name": "Apo Resettlement Phc",
+        "lga": "AMAC",
+        "address": "Xgj4+F44, Rabo, Madai Street, Apo Resettlement, Abuja",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9811375,
+        "longitude": 7.5053281
+    },
+    {
+        "name": "Kurudu Phc",
+        "lga": "AMAC",
+        "address": "Wgwv+64p, Kurudu 900109, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9455875,
+        "longitude": 7.5428281
+    },
+    {
+        "name": "Sheretti Phc",
+        "lga": "AMAC",
+        "address": "Wcrr+X7g, Abuja 900107, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9424375,
+        "longitude": 7.4407031
+    },
+    {
+        "name": "Piwoyi",
+        "lga": "AMAC",
+        "address": "X9XV+MF4, Clinic Road, Piwoyi 900108, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 8.9991375,
+        "longitude": 7.3937031
+    },
+    {
+        "name": "Family Health Clinic Area 2",
+        "lga": "AMAC",
+        "address": "Area 2, Garki 1, Abuja",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0322585,
+        "longitude": 7.4692408
+    },
+    {
+        "name": "Dei-Dei Bm Clinic",
+        "lga": "AMAC",
+        "address": "4775+WQ9, Deidei -Madalla Road, Opp Deidei Junction, Deidei, Madalla",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1147875,
+        "longitude": 7.2594844
+    },
+    {
+        "name": "Saburi Phc",
+        "lga": "AMAC",
+        "address": "475j+64x, Abuja 904104, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.1081125,
+        "longitude": 7.2803594
+    },
+    {
+        "name": "Gwagwa Phc",
+        "lga": "AMAC",
+        "address": "Gwagwa, Juwa 900106, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0752462,
+        "longitude": 7.3111562
+    },
+    {
+        "name": "Idu-Karmo Phc",
+        "lga": "AMAC",
+        "address": "39c5+Hrx, Idogwari, 900106, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0714875,
+        "longitude": 7.3596094
+    },
+    {
+        "name": "Gwarinpa Phc",
+        "lga": "AMAC",
+        "address": "3cx4+Jpg, Gwarinpa, 900108, Federal Capital Territory",
+        "days_of_immunization": "",
+        "hours_of_work": "",
+        "latitude": 9.0990625,
+        "longitude": 7.4068281
     }
 ];
